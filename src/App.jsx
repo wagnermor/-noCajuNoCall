f0751cd54056814daba311b8d99ba6d83ac43513
WagnerMor <wagnermor@outlook.com>
2023-03-21 17:39:02 -0300

    Add: Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import { Header } from './components/Header';
 import { Post } from './Post';
-import './global.css'
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+import './global.css';
 
 export function App() {
   const [count, setCount] = useState(0)
@@ -9,15 +12,19 @@ export function App() {
   return (
     <>
       <Header />
-      <h1>#noCajuNoCall</h1>
-      <Post 
-        author= 'Wagner Moreira'
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit ullam magni numquam omnis ipsa soluta molestias animi velit praesentium, facere iure cupiditate laudantium maxime beatae, esse sit? Officiis, repellendus atque?"
-      />
-      <Post
-        author='Wagneriano'
-        content='#noCajuNoCall'
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author= 'Wagner Moreira'
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit ullam magni numquam omnis ipsa soluta molestias animi velit praesentium, facere iure cupiditate laudantium maxime beatae, esse sit? Officiis, repellendus atque?"
+          />
+          <Post
+            author='Wagneriano'
+            content='#noCajuNoCall'
+          />
+        </main>
+      </div>
     </>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
-import styles from './Header.module.css'
+import styles from './Header.module.css';
+import cajuLogo from '../assets/caju-logo.png';
+
 export function Header() {
   return(
     <header className={styles.header}>
+      <img src={ cajuLogo } alt="" className = { styles.logo } />
       <h1> #noCajuNoCall </h1>
     </header>
   )
